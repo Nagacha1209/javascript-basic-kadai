d2e13b0052685524f27d6add5596a5744337975f
Nagacha1209 <nagachadrum1209@gmail.com>
2024-09-25 15:44:36 +0900

    変更

--- kadai_009/holiday.js
+++ kadai_009/holiday.js
@@ -2,7 +2,7 @@
 const holiDays = ['正月','成人の日','建国記念日','天皇誕生日','春分の日','昭和の日','憲法記念日','みどりの日','こどもの日','海の日','山の日','敬老の日','秋分の日','スポーツの日','文化の日','勤労感謝の日']
 
 //for文で配列の値をすべて出力する
-for (let i=0; i<=16; i++){
+for (let i=0; i<=15; i++){
     console.log(holiDays[i]);
 }
 
@@ -11,7 +11,7 @@ let i = 0
 
 console.log(holiDays[i]);
 
-while (i<=16){
+while (i<=15){
     i += 1;
     console.log(holiDays[i]);
 }
